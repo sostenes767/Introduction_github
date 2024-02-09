1b556afb6ed217958cb1daf336a232d5436d37e8
Sostenes <sjs.jeronimo99@gmail.com>
2024-02-09 12:53:38 -0300

    Initial commit

---
+++ Introduction_github.R
@@ -0,0 +1,22 @@
+library(usethis)
+
+use_git_config(user.name = "Sostenes",
+               user.email = "sjs.jeronimo99@gmail.com")
+
+
+# Criar uma senha para conectar o rstudio e github (token)
+
+create_github_token()
+
+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
+
+#Arquivo .Reviron
+edit_r_environ()
+
+#Vericando se tudo esta ok 
+usethis::git_sitrep()
+
+
+# Iniciando o Git  --------------------------------------------------------
+
+
